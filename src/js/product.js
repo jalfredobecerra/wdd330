f50f7526c00982e796a091f270feeba7d471ffb9
Julian <julian19becerra@gmail.com>
2026-05-01 17:54:09 -0500

    Completed

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,15 @@
 import { getLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam('product');
 
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
-
-console.log(await dataSource.findProductById(productId));
