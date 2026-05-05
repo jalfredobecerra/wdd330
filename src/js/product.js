6890319993a160a70f568763ecf81a51937d44d7
Julian <julian19becerra@gmail.com>
2026-05-05 16:41:15 -0500

    Completed.

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,8 @@
-import { getLocalStorage, getParam } from "./utils.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+
+loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
