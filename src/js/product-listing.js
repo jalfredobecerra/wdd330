cc4716e567abad95e384963570689be7507fc6f7
Julian <julian19becerra@gmail.com>
2026-05-15 23:11:43 -0500

    Fixes

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -2,13 +2,16 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
 
-loadHeaderFooter();
+async function init() {
+  await loadHeaderFooter();
+  const category = getParam("category");
+  const dataSource = new ProductData();
+  const element = document.querySelector(".product-list");
+  const search = getParam("search");
+  const searchTerm = category || search;
+  const productList = new ProductList(searchTerm, dataSource, element);
 
-const category = getParam("category");
-const dataSource = new ProductData();
-const element = document.querySelector(".product-list");
-const search = getParam("search");
-const searchTerm = category || search;
-const productList = new ProductList(searchTerm, dataSource, element);
+  productList.init();
+}
 
-productList.init();
+init();
